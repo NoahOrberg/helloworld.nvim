99240026b0cc5e6329d6e3c4058e37f76876bd18
NoahOrberg <noah.orberg.9999@gmail.com>
2017-12-12 20:22:42 +0900

    add command

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"strings"
 
+	"github.com/neovim/go-client/nvim"
 	"github.com/neovim/go-client/nvim/plugin"
 )
 
@@ -10,9 +11,14 @@ func hello(args []string) (string, error) {
 	return "Hello " + strings.Join(args, " "), nil
 }
 
+func helloCmd(v *nvim.Nvim, args []string) error {
+	return v.Command("echom 'hello " + args[0] + "'")
+}
+
 func main() {
 	plugin.Main(func(p *plugin.Plugin) error {
 		p.HandleFunction(&plugin.FunctionOptions{Name: "Hello"}, hello)
+		p.HandleCommand(&plugin.CommandOptions{Name: "HelloCmd", NArgs: "1"}, helloCmd)
 		return nil
 	})
 }
